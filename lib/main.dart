8e67f5ed585ce6f6480c48f279deb18a0b886595
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-10-10 20:29:33 +0200

    Added Firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'models/game_score.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final appDocumentDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
   Hive.registerAdapter(GameScoreAdapter());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(
     child: EverdellScore(),
   ));
